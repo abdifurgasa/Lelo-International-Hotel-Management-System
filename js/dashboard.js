484d1186c9a7457acb850d51e8262e4d5f85f8c9
abdifurgasa <abdifurgasa2nd@gmail.com>
2026-03-08 19:06:00 +0300

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -19,7 +19,24 @@ loadOrders();
 if(page==="order"){
 loadRoleOrders();
 }
+function loadDashboardStats(){
 
+let rooms = JSON.parse(localStorage.getItem("rooms")) || [];
+let foods = JSON.parse(localStorage.getItem("foods")) || [];
+let drinks = JSON.parse(localStorage.getItem("drinks")) || [];
+
+let totalRooms = rooms.length;
+
+let occupiedRooms = rooms.filter(r => r.status === "Occupied").length;
+
+document.getElementById("totalRooms").innerText = totalRooms;
+document.getElementById("occupiedRooms").innerText = occupiedRooms;
+document.getElementById("totalFoods").innerText = foods.length;
+document.getElementById("totalDrinks").innerText = drinks.length;
+
+}
+
+loadDashboardStats();
 function logout(){
 
 let confirmLogout=confirm("Are you sure you want to logout?");
